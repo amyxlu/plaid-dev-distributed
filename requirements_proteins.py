b5d81508d41306b563d81c3237e143ccf6761cd1
Amy X. Lu <amyxlu@berkeley.edu>
2023-10-11 23:09:37 +0000

    Update package dependencies for ESMFold and OpenFold

---
+++ requirements_proteins.py
@@ -0,0 +1,16 @@
+dm-tree
+modelcif
+ml_collections
+biopython
+omegaconf
+fair-esm
+
+# additionaly, run:
+# ```
+# git clone https://github.com/amyxlu/openfold.git
+# cd openfold
+# python setup.py develop
+# ```
+# This will enable imports
+# and compile the custom CUDA modules attention without importing the
+# other clunky openfold dependencies.
