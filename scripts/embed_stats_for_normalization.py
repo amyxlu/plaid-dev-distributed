d43fe57e5d0159e2449dfd8cbf5d8767167d6113
Amy X. Lu <amyxlu@berkeley.edu>
2023-12-07 07:05:02 +0000

    update some stats for normalization

--- scripts/embed_stats_for_normalization.py
+++ scripts/embed_stats_for_normalization.py
@@ -32,12 +32,12 @@ DATASET_TO_FASTA_FILE = {
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--lm_embedder_type", type=str, default="esmfold")
-    parser.add_argument("--dataset", type=str, default="uniref")
+    parser.add_argument("--dataset", type=str, default="cath")
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--n_val", type=int, default=5000)
-    parser.add_argument("--seq_len", type=int, default=512)
-    parser.add_argument("--min_len", type=int, default=32)
-    parser.add_argument("--suffix", type=str, default="dec1")
+    parser.add_argument("--seq_len", type=int, default=128)
+    parser.add_argument("--min_len", type=int, default=16)
+    parser.add_argument("--suffix", type=str, default="nov28")
     return parser.parse_args()
 
 
