6b3f517f8be7445fdb6c81bdf2ae482e3a4dc466
Amy X. Lu <amyxlu@berkeley.edu>
2023-12-07 07:05:31 +0000

    minor: delete old trainer

--- k_diffusion/__init__.py
+++ k_diffusion/__init__.py
@@ -1,2 +1,3 @@
 from . import callback, config, datasets, evaluation, external, gns, layers, models, sampling, utils, proteins, normalization
 from .layers import Denoiser
+from .trainer import Trainer
