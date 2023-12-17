ad7d04c0db40badbdfdce4abfef00cd3003a0684
Amy X. Lu <amyxlu@berkeley.edu>
2023-12-17 05:34:09 +0000

    update safe tensors script to save by train/val split

--- scripts/save_safe_tensors.py
+++ scripts/save_safe_tensors.py
@@ -1,11 +1,9 @@
-
 import torch
 import safetensors
 from tqdm import tqdm, trange
 import json
 
 from evo.dataset import FastaDataset
-from torch.utils.data import random_split
 import torch
 from pathlib import Path
 import h5py
@@ -13,30 +11,37 @@ import h5py
 import dataclasses
 
 import safetensors
-import k_diffusion as K
-from k_diffusion.models.esmfold import ESMFold
-import einops
+import plaid as K
+from plaid.esmfold import ESMFold
 import time
 
 
 @dataclasses.dataclass
 class ShardConfig:
     fasta_file: str = "/shared/amyxlu/data/cath/cath-dataset-nonredundant-S40.atom.fa"
-    output_dir: str = "/shared/amyxlu/data/cath/shards/"
+    train_output_dir: str = "/shared/amyxlu/data/cath/shards/train"
+    val_output_dir: str = "/shared/amyxlu/data/cath/shards/val"
     batch_size: int = 256 
-    max_seq_len: int = 256
-    min_seq_len: int = 30
+    max_seq_len: int = 64
+    min_seq_len: int = 16
     num_workers: int = 4
     num_batches_per_shard: int = 20
     compression: str = "safetensors"  # "safetensors", "hdf5"
+    shard: bool = False
+    train_frac: float = 0.8
 
 
-def make_fasta_dataloader(fasta_file, batch_size, num_workers=4):
+def make_fasta_dataloaders(fasta_file, batch_size, num_workers=4):
     # for loading batches into ESMFold and embedding
     ds = FastaDataset(fasta_file, cache_indices=True)
-    return torch.utils.data.DataLoader(
-        ds, batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=False
+    train_ds, val_ds = torch.utils.data.random_split(ds, [0.8, 0.2])
+    train_dataloader = torch.utils.data.DataLoader(
+        train_ds, batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=False
     )
+    val_dataloader = torch.utils.data.DataLoader(
+        val_ds, batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=False
+    )
+    return train_dataloader, val_dataloader
 
 
 def embed_batch(esmfold, sequences, max_len=512, min_len=30):
@@ -92,24 +97,13 @@ def save_h5_embeddings(embs, seq_lens, shard_number, outdir):
     del embs, seq_lens
 
 
-def main(cfg: ShardConfig):
-    start = time.time()
-    print("making esmfold...")
-    esmfold = ESMFold(make_trunk=False)
-    end = time.time()
-    print(f"done making esmfold in {end - start:.2f} seconds.")
+def run(dataloader, esmfold, output_dir, cfg: ShardConfig):
+    if cfg.shard:
+        num_shards = len(dataloader) // cfg.num_batches_per_shard + 1
+    else:
+        num_shards = 1
 
-    device = torch.device("cuda")
-    esmfold.to(device)
-    esmfold.eval()
-    esmfold.requires_grad_(False)
-
-
-    print("making dataloader")
-    dataloader = make_fasta_dataloader(cfg.fasta_file, cfg.batch_size, cfg.num_workers)
-    num_shards = len(dataloader) // cfg.num_batches_per_shard + 1
-
-    outdir = Path(cfg.output_dir) / f"seqlen_{cfg.max_seq_len}"
+    outdir = Path(output_dir) / f"seqlen_{cfg.max_seq_len}"
     if not outdir.exists():
         outdir.mkdir(parents=True)
     
@@ -125,6 +119,28 @@ def main(cfg: ShardConfig):
             save_h5_embeddings(emb_shard, seq_lens, shard_number, outdir)
         else:
             raise ValueError(f"invalid compression type {cfg.compression}")
+
+
+def main(cfg: ShardConfig):
+    start = time.time()
+    print("making esmfold...")
+    esmfold = ESMFold(make_trunk=False)
+    end = time.time()
+    print(f"done making esmfold in {end - start:.2f} seconds.")
+
+    device = torch.device("cuda")
+    esmfold.to(device)
+    esmfold.eval()
+    esmfold.requires_grad_(False)
+
+    print("making dataloader")
+    train_dataloader, val_dataloader = make_fasta_dataloaders(cfg.fasta_file, cfg.batch_size, cfg.num_workers)
+
+    print("creating train dataset")
+    run(train_dataloader, esmfold, cfg.train_output_dir, cfg)
+
+    print("creating val dataset")
+    run(val_dataloader, esmfold, cfg.val_output_dir, cfg)
 
 
 if __name__ == "__main__":
