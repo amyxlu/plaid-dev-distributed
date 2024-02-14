899254291b2bb4e49411b09048a54d0e070e2762
amyxlu <amyxlu@berkeley.edu>
2024-02-14 23:02:30 +0000

    misc changes

--- slurm/agent_sweep_diffusion.sh
+++ slurm/agent_sweep_diffusion.sh
@@ -1,5 +1,5 @@
 # for wandb sweep: sweep.slrm specifies compute requirements for wandb agent project/agentid
-njobs=10
+njobs=12
 wandb_agent_id="7j1b2heo"
 for i in $(seq 1 $njobs); do
     sbatch /homefs/home/lux70/code/plaid/slurm/agent_sweep_diffusion.slrm $wandb_agent_id
