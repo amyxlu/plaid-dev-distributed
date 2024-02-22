9ba7a1c6a8b444d8bd02ef01e46cdbb844514faf
amyxlu <amyxlu@berkeley.edu>
2024-02-22 23:31:49 +0000

    Save sequence

--- scripts/save_embedding_sequence_pairs.sh
+++ scripts/save_embedding_sequence_pairs.sh
@@ -1,4 +1,7 @@
-python save_embedding_sequence_pairs.py --batch_size 256 --lm_embedder_type "esmfold_pre_mlp" 
+python save_embedding_sequence_pairs.py --batch_size 128 --lm_embedder_type "esmfold" --max_seq_len 512 \
+  --fasta_file /homefs/home/lux70/storage/data/cath/cath-dataset-nonredundant-S40.atom.fa \
+  --train_output_dir /homefs/home/lux70/storage/data/cath/shards/train \
+  --val_output_dir /homefs/home/lux70/storage/data/cath/shards/val
   # --fasta_file /homefs/home/lux70/storage/data/rocklin/rocklin_stable.fasta \
   # --train_output_dir /homefs/home/lux70/storage/data/rocklin/shards/train \
   # --val_output_dir /homefs/home/lux70/storage/data/rocklin/shards/val \
