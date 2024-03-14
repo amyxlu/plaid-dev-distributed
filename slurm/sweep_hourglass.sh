1b2d8c477dc38d999afd4f1e3762764f1aeb64b3
amyxlu <amyxlu@berkeley.edu>
2024-03-14 23:00:01 +0000

    VQVAE experiments

--- slurm/sweep_hourglass.sh
+++ slurm/sweep_hourglass.sh
@@ -18,4 +18,16 @@
 # sbatch train_hourglass.slrm "experiment=hvq/recons_simple.yaml ++hourglass.lr=5e-5 ++hourglass.lr_num_warmup_steps=1000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=2 ++trainer.gradient_clip_val=0.5"
 # sbatch train_hourglass.slrm "experiment=hvq/recons_simple.yaml ++hourglass.lr=5e-5 ++hourglass.lr_num_warmup_steps=1000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=4 ++trainer.gradient_clip_val=0.5"
 # sbatch train_hourglass.slrm "experiment=hvq/recons_simple.yaml ++hourglass.lr=5e-5 ++hourglass.lr_num_warmup_steps=1000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=8 ++trainer.gradient_clip_val=0.5"
-sbatch train_hourglass.slrm "experiment=hvq/recons_simple.yaml ++hourglass.lr=8e-5 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16 ++trainer.gradient_clip_val=0.5"
+# sbatch train_hourglass.slrm "experiment=hvq/recons_simple.yaml ++hourglass.lr=8e-5 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16 ++trainer.gradient_clip_val=0.5"
+
+
+# sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.n_e=65536 ++hourglass.lr=8e-5 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16 ++trainer.gradient_clip_val=0.5"
+# sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.n_e=131072 ++hourglass.lr=8e-5 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16 ++trainer.gradient_clip_val=0.5"
+# sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.n_e=262144 ++hourglass.lr=8e-5 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16 ++trainer.gradient_clip_val=0.5"
+# sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.n_e=524288 ++hourglass.lr=8e-5 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16 ++trainer.gradient_clip_val=0.5"
+
+sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.seq_len=512 ++hourglass.n_e=65536 ++hourglass.lr=1e-4 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16"
+sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.seq_len=512 ++hourglass.n_e=131072 ++hourglass.lr=1e-4 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=64 ++hourglass.downproj_factor=16"
+
+sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.seq_len=512 ++hourglass.n_e=65536 ++hourglass.lr=1e-4 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=128 ++hourglass.downproj_factor=8"
+sbatch train_hourglass.slrm "experiment=hvq/recons_small.yaml ++hourglass.seq_len=512 ++hourglass.n_e=131072 ++hourglass.lr=1e-4 ++hourglass.lr_num_warmup_steps=3000 ++hourglass.e_dim=128 ++hourglass.downproj_factor=8"
