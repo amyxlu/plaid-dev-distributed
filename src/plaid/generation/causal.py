a43f7240bd9fb4eb21582115c571bbfb5b050953
amyxlu <amyxlu@berkeley.edu>
2024-04-06 01:44:22 +0000

    causal token generation lightning module refinement

--- src/plaid/generation/causal.py
+++ src/plaid/generation/causal.py
@@ -3,63 +3,8 @@ import torch
 from plaid.datasets import TokenDataModule
 from plaid.utils import get_lr_scheduler
 
-# """
-# Set up
-# """
-# vocab_size = 128
-# bos_token_id = vocab_size + 1
-# eos_token_id = vocab_size + 2
-
-# llama_config = LlamaConfig(
-#     bos_token_id = bos_token_id,
-#     eos_token_id = eos_token_id,
-#     vocab_size = vocab_size,
-#     num_hidden_layers = 4,
-#     num_attention_heads = 8
-# )
-
-# print('loading llama...')
-# model = LlamaForCausalLM(llama_config)
-
-# device = torch.device('cuda')
-# model = model.to(device)
-
-
-"""
-Training
-"""
-# optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
-
-# for i, batch in enumerate(train_dataloader):
-#     if i > 3: break
-
-#     batch = batch.long()  # chartensor -> long
-#     batch = batch.to(device)
-#     batch = batch[:, :64, :]
-#     x = batch.reshape(batch.shape[0], -1)
-
-#     """ Sample data batch
-#     In [8]: x.shape
-#     Out[8]: torch.Size([4, 1024])
-
-#     In [9]: x
-#     Out[9]:
-#     tensor([[ 45,  89,  46,  ...,  97,  84,   4],
-#             [106,  35,  35,  ...,  50,  74,  26],
-#             [  0,  86,   6,  ...,  98,  74,  40],
-#             [  1,  86,   0,  ...,  47,  79,   3]], device='cuda:0')
-#     """
-#     output = model(input_ids=x, labels=x)
-#     pred_idx = output['logits'].argmax(-1)
-
-#     loss = output['loss']
-
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
-#     print(loss.item())
-
 import lightning as L
+
 
 class CausalLightningModule(L.LightningModule):
     def __init__(
