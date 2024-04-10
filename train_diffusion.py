09386a089d979e3803f8be9d1a0b078a7994a006
amyxlu <amyxlu@berkeley.edu>
2024-04-10 18:08:41 +0000

    Train script with callbacks

--- train_diffusion.py
+++ train_diffusion.py
@@ -18,6 +18,10 @@ def train(cfg: DictConfig):
     # general set up
     torch.set_float32_matmul_precision("medium")
 
+    log_cfg = OmegaConf.to_container(cfg, throw_on_missing=True, resolve=True)
+    if rank_zero_only.rank == 0:
+        print(OmegaConf.to_yaml(log_cfg))
+
     def to_load_sequence_constructor(cfg):
         if (cfg.diffusion.sequence_decoder_weight > 0.0) or (
             cfg.callbacks.sample.calc_perplexity
@@ -35,13 +39,21 @@ def train(cfg: DictConfig):
             return LatentToStructure()
         else:
             return None
+    
+    def to_load_uncompressor(cfg):
+        if isinstance(cfg.compression_model_id, str):
+            from plaid.compression.uncompress import UncompressContinuousLatent
+            return UncompressContinuousLatent(
+                cfg.compression_model_id,
+                cfg.compression_ckpt_dir
+            ) 
+        else:
+            return None
 
+    # maybe make sequence/structure constructors
     sequence_constructor = to_load_sequence_constructor(cfg)
     structure_constructor = to_load_structure_constructor(cfg)
-
-    log_cfg = OmegaConf.to_container(cfg, throw_on_missing=True, resolve=True)
-    if rank_zero_only.rank == 0:
-        print(OmegaConf.to_yaml(log_cfg))
+    uncompressor = to_load_uncompressor(cfg)
 
     # lightning data and model modules
     datamodule = hydra.utils.instantiate(cfg.datamodule)
@@ -56,50 +68,35 @@ def train(cfg: DictConfig):
         beta_scheduler=beta_scheduler,
         latent_scaler=latent_scaler,
         sequence_constructor=sequence_constructor,
-        structure_constructor=structure_constructor
+        structure_constructor=structure_constructor,
+        uncompressor=uncompressor
     )
 
-    job_id = wandb.util.generate_id()
+    # maybe use prior job id, else generate new ID
+    if cfg.resume_from_model_id is not None:
+        job_id = cfg.resume_from_model_id 
+    else:
+        job_id = wandb.util.generate_id() 
+    
+    # save config
     dirpath = Path(cfg.paths.checkpoint_dir) / "diffusion" / job_id
+    dirpath.mkdir(parents=False)
+    config_path = dirpath / "config.yaml"
+    if not config_path.exists():
+        OmegaConf.save(cfg, config_path)
 
+    # other callbacks
+    lr_monitor = hydra.utils.instantiate(cfg.callbacks.lr_monitor)
+    checkpoint_callback = hydra.utils.instantiate(cfg.callbacks.checkpoint, dirpath=dirpath)
     if not cfg.dryrun:
+        # this will automatically log to the same wandb page
         logger = hydra.utils.instantiate(cfg.logger, id=job_id)
-        # logger.watch(diffusion, log="all", log_graph=False)
+        # logger.watch(model, log="all", log_graph=False)
     else:
         logger = None
 
-    lr_monitor = hydra.utils.instantiate(cfg.callbacks.lr_monitor)
-    checkpoint_callback = hydra.utils.instantiate(cfg.callbacks.checkpoint, dirpath=dirpath)
-
     # todo: add a wasserstein distance callback
     outdir = Path(cfg.paths.artifacts_dir) / "samples" / job_id
-    import IPython;IPython.embed()
-
-
-
-    """
-    vvvv scratch vvvv
-    """
-
-    datamodule.setup("fit")
-    train_dataloader = datamodule.train_dataloader()
-    x, _, _ = next(iter(train_dataloader))
-    mask = torch.ones((x.shape[0], x.shape[1])).long()
-    loss, x_recons, _ = diffusion(x, mask)
-
-
-    from plaid.compression.uncompress import UncompressContinuousLatent
-    uncompressor = UncompressContinuousLatent("2024-03-30T18-24-23")
-    x_recons_uncompress = uncompressor.uncompress(x_recons)
-
-    sampled_compressed = diffusion.sample(x.shape, return_all_timesteps=False)
-
-    """
-    ^^^^ scratch ^^^^
-    """
-
-
-
 
     sample_callback = hydra.utils.instantiate(
         cfg.callbacks.sample,
@@ -116,9 +113,7 @@ def train(cfg: DictConfig):
     trainer = hydra.utils.instantiate(
         cfg.trainer,
         logger=logger,
-        # callbacks=[lr_monitor, checkpoint_callback, sample_callback],
-        callbacks=[lr_monitor, checkpoint_callback],
-        
+        callbacks=[lr_monitor, checkpoint_callback, sample_callback],
     )
     if rank_zero_only.rank == 0 and isinstance(trainer.logger, WandbLogger):
         trainer.logger.experiment.config.update({"cfg": log_cfg}, allow_val_change=True)
