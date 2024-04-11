c6ef9a0d2e3ab2380ff703dc78e7275d557adeed
amyxlu <amyxlu@berkeley.edu>
2024-04-11 04:00:19 +0000

    Diffusion on more compression models

--- scripts/save_compressed_embeddings.sh
+++ scripts/save_compressed_embeddings.sh
@@ -1,7 +1,7 @@
 python save_compressed_embeddings.py \
-    --compressor_model_id "2024-03-30T18-24-23" \
+    --compressor_model_id "tk3g5edg" \
     --batch_size 128 \
     --max_seq_len 512 \
-    --num_workers 0
-    # --fasta_file /homefs/home/lux70/storage/data/rocklin/rocklin_stable.fasta \
-    # --base_output_dir "/homefs/home/lux70/storage/data/rocklin/compressed/" \
+    --num_workers 0 \
+    --fasta_file /homefs/home/lux70/storage/data/rocklin/rocklin_stable.fasta \
+    --base_output_dir "/homefs/home/lux70/storage/data/rocklin/compressed/" \
--- slurm/sweep_hdiff.sh
+++ slurm/sweep_hdiff.sh
@@ -1,1 +1,3 @@
 sbatch train_diffusion.slrm experiment=hdif/dual_track/rocklin_tanh_512_8 
+sbatch train_diffusion.slrm experiment=hdif/dual_track/rocklin_tanh_256_32
+sbatch train_diffusion.slrm experiment=hdif/dual_track/rocklin_tanh_256_64
