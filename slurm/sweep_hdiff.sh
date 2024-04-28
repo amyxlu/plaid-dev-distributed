9be618ef68fefc28751870de3355a3ebaa604639
amyxlu <amyxlu@berkeley.edu>
2024-04-28 19:14:15 +0000

    experiment config organization

--- slurm/sweep_hdiff.sh
+++ slurm/sweep_hdiff.sh
@@ -1,10 +1,2 @@
-# sbatch train_diffusion.slrm experiment=hdif/pfam_dit ++diffusion.lr=1e-3 
-# sbatch train_diffusion.slrm experiment=hdif/pfam_dit ++trainer.gradient_clip_val=5
-# sbatch train_diffusion.slrm experiment=hdif/pfam_dit ++diffusion.sequence_decoder_weight=1.0
-# sbatch train_diffusion.slrm experiment=hdif/pfam_dit ++diffusion.lr=1e-2 
-# sbatch train_diffusion.slrm experiment=hdif/pfam_dit ++diffusion.lr=1e-5 
-# sbatch train_diffusion.slrm experiment=hdif/pfam_dit ++diffusion.x_downscale_factor=0.01
-# sbatch train_diffusion.slrm ++datamodule.max_seq_len=512 experiment=hdif/pfam_dit_toy ++trainer.limit_val_batches=0.0 
-# sbatch train_diffusion.slrm ++datamodule.max_seq_len=512 experiment=hdif/pfam_dit_toy ++trainer.limit_val_batches=0.0 ++denoiser.depth=12 ++trainer.gradient_clip_val=5
-sbatch train_diffusion.slrm experiment=hdif/pfam_udit ++trainer.limit_val_batches=0.0 ++denoiser.depth=21
+sbatch train_diffusion.slrm experiment=diffusion/dual_track/pfam_1M ++denoiser.num_blocks=3 ++datamodule.batch_size=4 ++logger.name="pfam_U-TSA_1M_d5_b16"
 
--- slurm/sweep_hourglass.sh
+++ slurm/sweep_hourglass.sh
@@ -1,20 +1,3 @@
 # sbatch train_hourglass.slrm resume_from_model_id=jzlv54wl
-sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=128 ++hourglass.shorten_factor=4 ++logger.name="pfam_tanh_128_8" ++hourglass.lr=8e-5 ++hourglass.lr_num_training_steps=600000 ++hourglass.lr_num_cycles=4
-# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=256 ++hourglass.shorten_factor=1 ++logger.name="pfam_tanh_512_4" ++hourglass.lr=1e-4
-# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=256 ++hourglass.shorten_factor=2 ++logger.name="pfam_tanh_256_4" ++hourglass.lr=1e-4
-# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=128 ++hourglass.shorten_factor=4 ++logger.name="pfam_tanh_128_4" ++hourglass.lr=1e-4
-
-# sbatch train_hourglass.slrm experiment=hvq/bounded/rocklin.yaml
-# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=32 ++hourglass.shorten_factor=2 ++logger.name="pfam_tanh_256_32"
-# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=16 ++hourglass.shorten_factor=2 ++logger.name="pfam_tanh_256_64"
-
-# sbatch train_hourglass.slrm experiment=hvq/fsq/pfam_full ++hourglass.fsq_levels=[16,16,16,16] ++logger.name="pfam_fsq_16_16_16_16" 
-# sbatch train_hourglass.slrm experiment=hvq/fsq/pfam_full ++hourglass.fsq_levels=[8,8,8,6,5] ++logger.name="pfam_fsq_86"  
-
-# sbatch train_hourglass.slrm experiment=hvq/bounded/cath_small.yaml ++hourglass.downproj_factor=128 ++logger.name="cath_tanh_512_8"
-# sbatch train_hourglass.slrm experiment=hvq/bounded/cath_small.yaml ++hourglass.downproj_factor=32 ++hourglass.shorten_factor=2 ++logger.name="cath_tanh_256_32"
-# sbatch train_hourglass.slrm experiment=hvq/bounded/cath_small.yaml ++hourglass.downproj_factor=16 ++hourglass.shorten_factor=2 ++logger.name="cath_tanh_256_64"
-
-# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=128 ++logger.name="pfam_tanh_512_8" ++hourglass.lr=6e-5
-# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=128 ++hourglass.shorten_factor=2 ++logger.name="pfam_tanh_256_8" ++hourglass.lr=6e-5
-# sbatch train_hourglass.slrm experiment=hvq/bounded/uniref_full.yaml ++hourglass.downproj_factor=128 ++logger.name="uniref_tanh_512_8" ++hourglass.lr=6e-5
+# sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=128 ++hourglass.shorten_factor=4 ++logger.name="pfam_tanh_128_8" ++hourglass.lr=8e-5 ++hourglass.lr_num_training_steps=600000 ++hourglass.lr_num_cycles=4
+sbatch train_hourglass.slrm experiment=hvq/bounded/pfam_full.yaml ++hourglass.downproj_factor=128 ++hourglass.shorten_factor=2 ++logger.name="pfam_tanh_256_8" ++hourglass.lr=6e-5
