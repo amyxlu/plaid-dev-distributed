27cf386355367894307e824e90ef5f1dce558e11
amyxlu <amyxlu@berkeley.edu>
2024-05-05 00:52:55 +0000

    fourier features ndim mismatch

--- src/plaid/denoisers/modules/embedders.py
+++ src/plaid/denoisers/modules/embedders.py
@@ -61,8 +61,11 @@ class FourierFeatures(nn.Module):
         assert out_features % 2 == 0
         self.register_buffer('weight', torch.randn([out_features // 2, in_features]) * std)
 
-    def forward(self, input):
-        f = 2 * math.pi * input @ self.weight.T
+    def forward(self, sigma):
+        if sigma.ndim < 2:
+            sigma = sigma.unsqueeze(1)
+
+        f = 2 * math.pi * sigma @ self.weight.T
         return torch.cat([f.cos(), f.sin()], dim=-1)
 
 
