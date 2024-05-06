5bc27cdda49b26d43982c2609497384ff35e7faf
amyxlu <amyxlu@berkeley.edu>
2024-05-06 18:58:51 +0000

    updates to EDM experiment sweeps

--- scripts/save_compressed_h5.sh
+++ scripts/save_compressed_h5.sh
@@ -3,12 +3,22 @@
 # 512 x 8: jzlv54wl
 # 256 x 8: qjs33lme
 
+# python save_compressed_h5.py \
+#     --compression_model_id "qjs33lme" \
+#     --compression_model_name "epoch0-step118000-2.495.ckpt" \
+#     --max_dataset_size -1 \
+#     --output_dir "/homefs/home/lux70/storage/data/pfam/compressed/subset_full_with_clan/fp16" \
+#     --max_seq_len 512 \
+#     --train_split_frac 0.999 \
+#     --float_type "fp16" \
+#     --batch_size 128 
+
 python save_compressed_h5.py \
-    --compression_model_id "qjs33lme" \
-    --compression_model_name "epoch0-step118000-2.495.ckpt" \
-    --max_dataset_size -1 \
-    --output_dir "/homefs/home/lux70/storage/data/pfam/compressed/subset_full_with_clan/fp16" \
+    --compression_model_id "j1v1wv6w" \
+    --compression_model_name "last.ckpt" \
+    --max_dataset_size 1000000 \
+    --output_dir "/homefs/home/lux70/storage/data/pfam/compressed/subset_1M_with_clan/fp16" \
     --max_seq_len 512 \
-    --train_split_frac 0.999 \
+    --train_split_frac 0.99 \
     --float_type "fp16" \
     --batch_size 128 
--- slurm/sweep_edm.sh
+++ slurm/sweep_edm.sh
@@ -1,4 +1,9 @@
-sbatch train_edm.slrm experiment=cfg/256x8_vd 
-# sbatch train_edm.slrm experiment=cfg/256x8_vd ++denoiser.use_self_conditioning=True
-sbatch train_edm.slrm experiment=cfg/256x8_cosine
-# sbatch train_edm.slrm experiment=cfg/256x8_cosine ++denoiser.use_self_conditioning=True
+# sbatch train_edm.slrm experiment=cfg/256x8_vd ++use_compile=False 
+# sbatch train_edm.slrm experiment=cfg/256x8_vd ++denoiser.use_self_conditioning=True ++use_compile=False
+# sbatch train_edm.slrm experiment=cfg/256x8_cosine ++use_compile=False
+# sbatch train_edm.slrm experiment=cfg/256x8_cosine ++denoiser.use_self_conditioning=True ++use_compile=False
+sbatch train_edm.slrm experiment=cfg/256x8_vd ++diffusion.lr=1e-5
+sbatch train_edm.slrm experiment=cfg/256x8_vd ++diffusion.lr=3e-5 ++diffusion.lr_sched_type=inverse_sqrt
+sbatch train_edm.slrm experiment=cfg/256x8_vd ++diffusion.lr=5e-5 ++diffusion.lr_sched_type=inverse_sqrt
+sbatch train_edm.slrm experiment=cfg/256x8_vd ++diffusion.lr=3e-5 ++diffusion.lr_sched_type=linear
+sbatch train_edm.slrm experiment=cfg/256x8_cosine ++diffusion.lr=1e-5
