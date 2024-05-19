7f88a762a76665a4c4581df9716d22df3fa52690
amyxlu <amyxlu@berkeley.edu>
2024-05-19 20:56:40 +0000

    more benchmark experiments

--- slurm/run_benchmark_noshorten.sh
+++ slurm/run_benchmark_noshorten.sh
@@ -1,4 +1,13 @@
+# task=ss
+# for id in 1b64t79h 1hr1x9r5 yfel5fnl v2cer77t 2tjrgcde 3rs1hxky 5z4iaak9 q3m9fhii; do
+#     sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
+# done
 
-task=contact
-id=5z4iaak9
-python run_benchmark.py --config-name contact ++task.model.compression_model_id=5z4iaak9
+# id=q3m9fhii
+# id=1b64t79h
+# id=1hr1x9r5
+id=yfel5fnl
+
+for task in contact ss pdbbind bindingdb ppi_affinity yeast human; do
+    sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
+done
