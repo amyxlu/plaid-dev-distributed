7f24820d795cfbb14b7a868580936f726b443ecc
amyxlu <amyxlu@berkeley.edu>
2024-05-30 09:43:14 +0000

    hourglass yaml and launch script updates

--- slurm/run_benchmark_noshorten.sh
+++ slurm/run_benchmark_noshorten.sh
@@ -1,27 +1,29 @@
-# for id in 1b64t79h 1hr1x9r5 yfel5fnl v2cer77t 2tjrgcde 3rs1hxky 5z4iaak9 q3m9fhii; do
+for id in 1b64t79h yfel5fnl v2cer77t 3rs1hxky; do
+    for task in "human" "yeast" "ss" "contact"; do
+        sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
+    done
+done
+
+
+# task="human"
+# for id in 2tjrgcde 5z4iaak9 q3m9fhii; do
 #     sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
 # done
 
 
-task="human"
-for id in 2tjrgcde 5z4iaak9 q3m9fhii; do
-    sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
-done
+# task="yeast"
+# for id in 2tjrgcde 5z4iaak9 q3m9fhii; do
+#     sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
+# done
 
 
-task="yeast"
-for id in 2tjrgcde 5z4iaak9 q3m9fhii; do
-    sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
-done
+# task="contact"
+# for id in 3rs1hxky 5z4iaak9 q3m9fhii; do
+#     sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
+# done
 
 
-task="contact"
-for id in 3rs1hxky 5z4iaak9 q3m9fhii; do
-    sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
-done
-
-
-task="ss"
-for id in 1hr1x9r5 3rs1hxky 5z4iaak9 q3m9fhii; do
-    sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
-done
+# task="ss"
+# for id in 1hr1x9r5 3rs1hxky 5z4iaak9 q3m9fhii; do
+#     sbatch run_benchmark.slrm --config-name $task ++task.model.compression_model_id=$id; sleep 10 
+# done
--- slurm/sweep_hdiff.sh
+++ slurm/sweep_hdiff.sh
@@ -1,8 +1,3 @@
-# sbatch train_diffusion.slrm experiment=hdif/pfam_dit ++datamodule.num_workers=16 
-# sbatch train_diffusion.slrm experiment=hdif/pfam_clan ++datamodule.num_workers=16 ++callbacks.sample.batch_size=32 ++diffusion.lr=1e-5 
-# sbatch train_diffusion.slrm experiment=hdif/pfam_clan ++resume_from_model_id='2dj696qw'
-# sbatch train_diffusion.slrm experiment=hdif/pfam_clan ++resume_from_model_id='17lwgwcf'
-
-for id in g8e83omk 7str7fhl ich20c3q uhg29zk4 13lltqha fbbrfqzk kyytc8i9 mm9fe6x9 8ebs7j9h; do
-    sbatch train_diffusion.slrm experiment=hdif/cath_dit ++compression_model_id=$id
+for id in e2iwuio0 t1ji0n45 0u6sw28c 5006fhbi 6nahhz22 ftqrhze9 xppmuppx gzkug3bi; do
+    sbatch train_diffusion.slrm resume_from_model_id=$id
 done
