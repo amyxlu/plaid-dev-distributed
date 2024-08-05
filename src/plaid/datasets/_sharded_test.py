b9c72e3676608cb837333a6c5be7137ab7294dbe
amyxlu <amyxlu@berkeley.edu>
2024-08-05 14:08:00 +0000

    minor: shard test updates

--- src/plaid/datasets/_sharded_test.py
+++ src/plaid/datasets/_sharded_test.py
@@ -2,6 +2,7 @@
 
 if __name__ == "__main__":
     from plaid.datasets import FunctionOrganismDataModule
+    import torch
 
     datamodule = FunctionOrganismDataModule(
         train_shards="/data/lux70/data/pfam/compressed/j1v1wv6w/train/shard{000..160}.tar",
@@ -10,27 +11,33 @@ if __name__ == "__main__":
         go_metadata_fpath="/data/lux70/data/pfam/pfam2go.csv",
         organism_metadata_fpath="/data/lux70/data/pfam/organism_counts.csv",
         cache_dir="/homefs/home/lux70/cache/plaid_data_cache/j1v1wv6w",
-        train_epoch_num_batches=50_000,
+        train_epoch_num_batches=100_000,
         val_epoch_num_batches=1_000,
         shuffle_buffer=10_000,
         shuffle_initial=10_000,
-        num_workers=2,
+        num_workers=4,
     )
     datamodule.setup()
 
     train_dataloader = datamodule.train_dataloader()
     train_dataset = datamodule.train_ds
 
+
     for i, batch in enumerate(train_dataloader):
         if i > 20: 
             break
+        embedding, mask, go_idx, organism_idx, pfam_id, sample_id, local_path = batch
         print(batch[2:5])
+        assert embedding.shape == torch.Size([64, 512, 32])
+        assert mask.shape == torch.Size([64, 512])
+        assert go_idx.shape == torch.Size([64])
+        assert organism_idx.shape == torch.Size([64])
         print("\n\n")
         
-    val_dataloader = datamodule.val_dataloader()
-    for i, batch in enumerate(val_dataloader):
-        if i > 20: 
-            break
-        print(batch[-2:])
-        print("\n\n")
+    # val_dataloader = datamodule.val_dataloader()
+    # for i, batch in enumerate(val_dataloader):
+    #     if i > 20: 
+    #         break
+    #     print(batch[-2:])
+    #     print("\n\n")
         
