57b859db29f71cc84cb2b118161c59dff339ecda
amyxlu <amyxlu@berkeley.edu>
2024-08-12 02:06:24 +0000

    try slurm config fixes

--- slurm/run_diffusion_slrm.py
+++ slurm/run_diffusion_slrm.py
@@ -4,35 +4,39 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_gpus", type=int, default=1)
-parser.add_argument("--n_nodes", type=int, default=1)
-parser.add_argument("--n_cpus", type=int, default=16)
-parser.add_argument("--flags", type=str, default="")
+parser.add_argument("-g", "--n_gpus_per_node", type=int, default=1)
+parser.add_argument("-n", "--n_nodes", type=int, default=1)
+parser.add_argument("-c", "--n_cpus_per_task", type=int, default=12)
+parser.add_argument("-f", "--flags", type=str, default="")
 args = parser.parse_args()
 
 flags = ""
+
 flags += args.flags
 
 defaults = f"""#!/usr/bin/env bash
+#SBATCH --partition gpu2
 #SBATCH --nodes {args.n_nodes} 
-#SBATCH --gpus {args.n_gpus}
-#SBATCH --ntasks-per-node {args.n_gpus // args.n_nodes}
-#SBATCH --mem-per-cpu=8G
-#SBATCH -p gpu2
-#SBATCH -c {args.n_cpus} 
+#SBATCH --ntasks-per-node {args.n_gpus_per_node} 
+#SBATCH --gpus-per-node {args.n_gpus_per_node}
+#SBATCH --cpus-per-task {args.n_cpus_per_task}
 #SBATCH --time=10-00:00:00
 #SBATCH --job-name train 
 
-eval "$(conda shell.bash hook)"
-conda activate plaid 
+set -ex
+
+source ~/.bashrc
+micromamba activate plaid 
 
 export HYDRA_FULL_ERROR=1
-export TOKENIZERS_PARALLELISM=false
-export NCCL_DEBUG=INFO
+export NCCL_DEBUG=TRACE
 cd /homefs/home/lux70/code/plaid/
 
 nvidia-smi
-srun python train_compositional.py {flags}
+
+srun python train_compositional.py {flags} \
+    ++trainer.devices=$SLURM_GPUS_PER_NODE \
+    ++trainer.num_nodes=$SLURM_JOB_NUM_NODES
 """
 
 hashid = uuid.uuid4().hex[:7]
