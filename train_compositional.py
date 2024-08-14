83c9f8f2ec280a4039d800a134bc12b9d94f844b
amyxlu <amyxlu@berkeley.edu>
2024-08-14 16:19:01 +0000

    more hacks to resume from old runs

--- train_compositional.py
+++ train_compositional.py
@@ -21,19 +21,16 @@ from plaid.diffusion import FunctionOrganismDiffusion
 _PROJECT_NAME = "plaid_compositional_conditioning"
 
 
-def delete_key(cfg: OmegaConf, key: str = "_target_"):
-    cfg = cfg.__delattr__(key)
-    return cfg
+def delete_key(d: dict, key: str = "_target_") -> dict:
+    if key in d:
+        d.pop(key)
+    return d
 
 
 def maybe_resume(cfg: OmegaConf, project_name: str) -> T.Tuple[dict, str, Path, bool]:
     # maybe use prior job id, else generate new ID
-    if cfg.resume_from_model_id is not None:
-        job_id = cfg.resume_from_model_id
-        is_resumed = True
-    else:
-        job_id = wandb.util.generate_id()
-        is_resumed = False
+    is_resumed = cfg.resume_from_model_id is not None
+    job_id = cfg.resume_from_model_id if is_resumed else wandb.util.generate_id()
 
     # set up checkpoint and config yaml paths
     outdir = Path(cfg.paths.checkpoint_dir) / project_name / job_id
@@ -49,17 +46,22 @@ def maybe_resume(cfg: OmegaConf, project_name: str) -> T.Tuple[dict, str, Path, 
             if not config_path.exists():
                 OmegaConf.save(cfg, config_path)
 
-    log_cfg = OmegaConf.to_container(cfg, throw_on_missing=True, resolve=True)
-    rank_zero_info(OmegaConf.to_yaml(log_cfg))
-    return log_cfg, job_id, outdir, is_resumed
+    # backwards compatibility:
+    if hasattr(cfg.trainer, "precision"):
+        if cfg.trainer.precision == "bf16-mixed":
+            cfg.trainer.update({"precision": "32"})
+
+    return cfg, job_id, outdir, is_resumed
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="train_compositional")
-def train(cfg: DictConfig):
+def train(cfg: DictConfig) -> None:
     import torch
     torch.set_float32_matmul_precision("medium")
 
-    log_cfg, job_id, outdir, is_resumed = maybe_resume(cfg, _PROJECT_NAME)
+    cfg, job_id, outdir, is_resumed = maybe_resume(cfg, _PROJECT_NAME)
+    log_cfg = OmegaConf.to_container(cfg, throw_on_missing=True, resolve=True)
+    rank_zero_info(OmegaConf.to_yaml(log_cfg))
 
     ####################################################################################################
     # Data and model setup  
@@ -69,15 +71,23 @@ def train(cfg: DictConfig):
     input_dim = constants.COMPRESSION_INPUT_DIMENSIONS[cfg.compression_model_id]
     shorten_factor = constants.COMPRESSION_SHORTEN_FACTORS[cfg.compression_model_id]
 
-    if not is_resumed:
-        datamodule = hydra.utils.instantiate(cfg.datamodule)
-        denoiser = hydra.utils.instantiate(cfg.denoiser, input_dim=input_dim)
-        diffusion = hydra.utils.instantiate(cfg.diffusion, model=denoiser)
+    datamodule = hydra.utils.instantiate(cfg.datamodule)
+
+    if is_resumed:
+        denoiser_cfg = delete_key(OmegaConf.to_container(cfg.denoiser), "_target_")
+        diffusion_cfg = delete_key(OmegaConf.to_container(cfg.diffusion), "_target_")
+
+        denoiser = FunctionOrganismDiT(**denoiser_cfg, input_dim=input_dim)
+        denoiser = torch.compile(denoiser)
+
+        # backwards compatibility:
+        outdated = hasattr(cfg, "use_old_ema_module")
+        diffusion = FunctionOrganismDiffusion(**diffusion_cfg, model=denoiser, use_old_ema_module=outdated)
+    
     else:
-        import IPython;IPython.embed()
-        datamodule = FunctionOrganismDataModule(**delete_key(cfg.datamodule)) 
-        denoiser = FunctionOrganismDiT(**delete_key(cfg.denoiser), input_dim=input_dim)
-        diffusion = FunctionOrganismDiffusion(**delete_key(cfg.diffusion), model=denoiser)
+        denoiser = hydra.utils.instantiate(cfg.denoiser, input_dim=input_dim)
+        denoiser = torch.compile(denoiser)
+        diffusion = hydra.utils.instantiate(cfg.diffusion, model=denoiser)
 
     # logging details
     trainable_parameters = count_parameters(diffusion, require_grad_only=True)
