6e286a316903641b535f7cbc699141bcc47981d7
amyxlu <amyxlu@berkeley.edu>
2024-08-17 17:08:31 +0000

    fix

--- src/plaid/denoisers/modules/__init__.py
+++ src/plaid/denoisers/modules/__init__.py
@@ -1,4 +1,4 @@
-from .embedders import GaussianFourierProjection, Base2FourierFeatures, TimestepEmbedder
+from .embedders import GaussianFourierProjection, Base2FourierFeatures, TimestepEmbedder, LabelEmbedder
 from .rope import RotaryEmbedding
 from ._base_block import BaseBlock
 from ._base_denoiser import BaseDenoiser
