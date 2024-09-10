30b61e7af124c805f3442c9e35c43207346032c4
amyxlu <amyxlu@berkeley.edu>
2024-09-10 18:51:40 +0000

    minor training related

--- slurm/sweep_diffusion.sh
+++ slurm/sweep_diffusion.sh
@@ -16,12 +16,14 @@ python run_diffusion_slrm.py --n_gpus 4 --flags "experiment=udit/B ++logger.name
 # F = no cond drop
 python run_diffusion_slrm.py --n_gpus 4 --flags "experiment=udit/B ++logger.name='AblationF__NoCondDrop' ++diffusion.function_y_cond_drop_prob=0.0 ++diffusion.organism_y_cond_drop_prob=0.0"
 
-# python run_diffusion_slrm.py -g 4 -n 1 -c 8 --flags "++resume_from_model_id=lqp25b7g"
+python run_diffusion_slrm.py -g 4 -n 1 -c 8 --flags "++resume_from_model_id=lqp25b7g"
 # python run_diffusion_slrm.py -g 8 -n 2 -c 8 --flags "++resume_from_model_id=4hdab8dn"
 # python run_diffusion_slrm.py -g 8 -n 5 -c 8 --flags "++resume_from_model_id=5j007z42"
 # python run_diffusion_slrm.py -g 8 -n 2 -c 8 --flags "++resume_from_model_id=4hdab8dn"
-# python run_diffusion_slrm.py -g 4 -n 1 -c 8 --flags "++resume_from_model_id=ksme77o6"
+python run_diffusion_slrm.py -g 4 -n 1 -c 8 --flags "++resume_from_model_id=ksme77o6"
 # python run_diffusion_slrm.py -g 4 -n 1 -c 8 --flags "++resume_from_model_id=lqp25b7g"
 
 # python run_diffusion_slrm.py -g 8 -n 10 -c 8 --flags "experiment=udit/XXL ++logger.name=UDiT_XXL"
 
+
+# python run_diffusion_slrm.py -g 8 -n 7 -c 8 --flags "++resume_from_model_id=5j007z42"
