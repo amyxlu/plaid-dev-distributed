affecad30f2c331e4ba4148b5bd7a68e1f1ab93c
amyxlu <amyxlu@berkeley.edu>
2024-09-11 21:01:51 +0000

    sample function conditionally

---
+++ scripts/eval/sample_function_conditional.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from omegaconf import OmegaConf, DictConfig
+
+from plaid.utils import round_to_multiple
+from plaid.pipeline import SampleLatent
+
+"""
+Script configs
+"""
+
+NUM_TO_EVAL = 10
+
+script_config = {
+    "return_all_timesteps": True,
+    "num_samples": 512
+}
+
+cfg = OmegaConf.load("/homefs/home/lux70/code/plaid/configs/pipeline/sample_latent.yaml")
+cfg.pop("defaults")
+
+"""
+Get unique GO terms in val dataset
+"""
+df = pd.read_parquet("/data/lux70/data/pfam/val.parquet")
+unique_go_idxs = df.GO_idx.unique()
+unique_go_idxs.sort()
+
+"""
+Loop through
+"""
+for i, idx in enumerate(unique_go_idxs):
+    if i > NUM_TO_EVAL:
+        break
+
+    median_len = int(df[df.GO_idx == idx].seq_len.median())
+    sample_len = round_to_multiple(median_len / 2, multiple=4)
+
+    for k, v in script_config.items():
+        cfg[k] = v
+
+    # for each unique GO term, keep organism as the "unconditional" index and use median length
+    cfg.function_idx = int(idx)
+    cfg.length = int(sample_len)
+
+    sample_latent = SampleLatent(**cfg)
+    sample_latent.run()
--- src/plaid/pipeline/__init__.py
+++ src/plaid/pipeline/__init__.py
@@ -2,9 +2,3 @@ from ._decode import DecodeLatent
 from ._fold import FoldPipeline
 from ._sample import SampleLatent
 from ._inverse_fold import InverseFoldPipeline
-
-
-# from .run_decode import run as run_decode
-# from .run_fold import run as run_fold
-# from .run_sample import run as run_sample
-# from .run_inverse_fold import run as run_inverse_fold
