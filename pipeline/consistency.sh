d3c303e1b8a308b56c04beb6c3028284e1ea4920
amyxlu <amyxlu@berkeley.edu>
2024-09-17 05:02:16 +0000

    tmp sh script

--- pipeline/consistency.sh
+++ pipeline/consistency.sh
@@ -6,12 +6,23 @@ SAMPLE_ARTIFACTS_ROOT_DIR="/data/lux70/plaid/artifacts/samples"
 # f308_o3617 f349_o3617 f356_o3617 f307_o3617
 
 # for COND_CODE in f322_o3617 f362_o3617 f263_o3617 f319_o3617 f269_o3617 f327_o3617 f308_o3617 f349_o3617 f356_o3617 f307_o3617; do
+# for COND_CODE in f308_o3617 f349_o3617 f356_o3617 f307_o3617; do
 
-for COND_CODE in f322_o3617 f362_o3617 f263_o3617; do 
+cd ${SAMPLE_ARTIFACTS_ROOT_DIR}/${PLAID_MODEL_ID}/
+
+condcodes=($(find . -maxdepth 1 -type d -name 'f*o*' -not -path .))
+
+for COND_CODE in "${condcodes[@]}"; do
+    COND_CODE=${COND_CODE#./}
+    echo $COND_CODE
+
     cd ${SAMPLE_ARTIFACTS_ROOT_DIR}/${PLAID_MODEL_ID}/${COND_CODE}/
 
+    # just do get some cleaning out of the way too
+    rm -rf outputs
+
     # Get a list of subdirectories in the current directory
-    subdirs=($(find . -maxdepth 1 -type d -not -path .))
+    subdirs=($(find . -maxdepth 1 -type d -name '24*' -not -path .))
 
     # Check the number of subdirectories
     if [ ${#subdirs[@]} -eq 1 ]; then
@@ -19,12 +30,11 @@ for COND_CODE in f322_o3617 f362_o3617 f263_o3617; do
         NPZ_TIMESTAMP="${subdirs[0]#./}"
     else
         # More than one subdirectory; find the most recently created one
-        NPZ_TIMESTAMP=$(ls -dt "${subdirs[@]}" | head -n 1)
-        NPZ_TIMESTAMP="${TIMESTAMP#./}"
+        NPZ_TIMESTAMP=$(ls -dt "${subdirs[@]}" | tail -n 1)
+        NPZ_TIMESTAMP="${NPZ_TIMESTAMP#./}"
     fi
 
-    # Output the TIMESTAMP
-    echo "NPZ_TIMESTAMP is set to: $NPZ_TIMESTAMP"
+    echo NPZ_TIMESTAMP is set to: $NPZ_TIMESTAMP
 
     python /homefs/home/lux70/code/plaid/pipeline/run_decode.py \
         ++npz_path=${SAMPLE_ARTIFACTS_ROOT_DIR}/${PLAID_MODEL_ID}/${COND_CODE}/${NPZ_TIMESTAMP}/latent.npz \
@@ -42,6 +52,7 @@ for COND_CODE in f322_o3617 f362_o3617 f263_o3617; do
 
     python /homefs/home/lux70/code/plaid/pipeline/run_fold.py \
         ++fasta_file=${SAMPLE_ARTIFACTS_ROOT_DIR}/${PLAID_MODEL_ID}/${COND_CODE}/${NPZ_TIMESTAMP}/generated/sequences.fasta \
-        ++outdir=${SAMPLE_ARTIFACTS_ROOT_DIR}/${PLAID_MODEL_ID}/${COND_CODE}/${NPZ_TIMESTAMP}/inverse_generate/structures
+        ++outdir=${SAMPLE_ARTIFACTS_ROOT_DIR}/${PLAID_MODEL_ID}/${COND_CODE}/${NPZ_TIMESTAMP}/inverse_generate/structures \
+        ++batch_size=32
 done
 
