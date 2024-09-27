700e41075c91f697fa3328d2082e1feffb31d063
amyxlu <amyxlu@berkeley.edu>
2024-09-27 22:01:20 +0000

    change some consistency calculation defaults

--- consistency_only_pipeline.py
+++ consistency_only_pipeline.py
@@ -1,26 +1,11 @@
-from typing import Optional
 from pathlib import Path
-import glob
 import os
-import time
 
-import wandb
-import pandas as pd
 from omegaconf import DictConfig, OmegaConf
 import torch
-import numpy as np
-from safetensors import safe_open
 import hydra
 
 from plaid.esmfold import esmfold_v1
-from plaid.evaluation import (
-    parmar_fid,
-    RITAPerplexity,
-    CrossConsistencyEvaluation,
-    SelfConsistencyEvaluation,
-)
-from plaid.typed import PathLike
-from plaid.utils import extract_avg_b_factor_per_residue, parse_sequence_from_structure
 
 
 def default(val, default_val):
@@ -82,149 +67,64 @@ def phantom_generate_sequence_run(
 
 @hydra.main(config_path="/homefs/home/lux70/code/plaid/configs/pipeline", config_name="consistency")
 def main(cfg: DictConfig):
+    print(OmegaConf.to_yaml(cfg))
+    
     inverse_generate_sequence_cfg = cfg.inverse_generate_sequence
     inverse_generate_structure_cfg = cfg.inverse_generate_structure
     phantom_generate_sequence_cfg = cfg.phantom_generate_sequence
 
     outdir = Path(cfg.samples_dir)
 
-    wandb.init(
-        project="plaid-sampling",
-        name=cfg.wandb_job_name
-    )
+    # ===========================
+    # Inverse generations for cross-consistency
+    # ===========================
 
-    perplexity_calc = RITAPerplexity()
+    if cfg.run_inverse:
+        if not (outdir / "inverse_generated").exists():
+            Path(outdir / "inverse_generated").mkdir(parents=True)
 
-    samples_d = {
-        "structure": [],
-        "sequence": [],
-        "perplexity": [],
-    }
+        # run ProteinMPNN for generated structures
+        input_pdb_dir = outdir / "generated/structures"
+        output_fasta_path = outdir / "inverse_generated/sequences.fasta"
+        inverse_generate_sequence_run(
+            inverse_generate_sequence_cfg,
+            pdb_dir=input_pdb_dir,
+            output_fasta_path=output_fasta_path,
+        )
+        esmfold = esmfold_v1()
+        esmfold.eval().requires_grad_(False)
+        esmfold.cuda()
 
-    pdb_paths = glob.glob(str(outdir / "generated/structures/*.pdb"))
-    seq_strs = [parse_sequence_from_structure(pdb_path=pdb_path) for pdb_path in pdb_paths]
-    num_samples = len(pdb_paths)
 
-    for i in range(num_samples):
-        seqstr, pdbpath = seq_strs[i], pdb_paths[i]
-        samples_d["structure"].append(wandb.Molecule(str(pdbpath)))
-        samples_d["sequence"].append(seqstr)
-
-        # calculate perplexity
-        perplexity = perplexity_calc.calc_perplexity(seqstr)
-        samples_d["perplexity"].append(perplexity)
-
-    esmfold = esmfold_v1()
-    esmfold.eval().requires_grad_(False)
-    esmfold.cuda()
-
-    # run ProteinMPNN for generated structures
-    input_pdb_dir = outdir / "generated/structures"
-    output_fasta_path = outdir / "inverse_generated/sequences.fasta"
-    inverse_generate_sequence_run(
-        inverse_generate_sequence_cfg,
-        pdb_dir=input_pdb_dir,
-        output_fasta_path=output_fasta_path,
-    )
-
-    # run ESMFold for generated sequences
-    input_fasta_file = outdir / "generated" / "sequences.fasta"
-    structure_outdir = outdir / "inverse_generated" / "structures"
-    inverse_generate_structure_run(
-        inverse_generate_structure_cfg,
-        fasta_file=input_fasta_file,
-        outdir=structure_outdir,
-        esmfold=esmfold,
-    )
-
-    cross_consistency_calc = CrossConsistencyEvaluation(outdir)
-
-    # should be already sorted in the same order
-    ccrmsd = cross_consistency_calc.cross_consistency_rmsd()
-    # ccrmspd = cross_consistency_calc.cross_consistency_rmspd()
-    cctm = cross_consistency_calc.cross_consistency_tm()
-    ccsr = cross_consistency_calc.cross_consistency_sr()
-
-    wandb.log(
-        {
-            # detailed stats for ccRMSD and ccSR as representative metrics
-            "ccrmsd_mean": np.mean(ccrmsd),
-            "ccrmsd_std": np.std(ccrmsd),
-            "ccrmsd_median": np.std(ccrmsd),
-            "ccsr_mean": np.mean(ccsr),
-            "ccsr_std": np.std(ccsr),
-            "ccsr_median": np.std(ccsr),
-            # also log histogram
-            "cctm_hist": wandb.Histogram(cctm),
-            "ccrmsd_hist": wandb.Histogram(ccrmsd),
-            # "ccrmspd_hist": wandb.Histogram(ccrmspd),
-            "ccsr_hist": wandb.Histogram(ccsr),
-        }
-    )
-
-    # add to the big table
-
-    samples_d["ccrmsd"] = ccrmsd
-    # samples_d["ccrmspd"] = ccrmspd
-    samples_d["cctm"] = cctm
-    samples_d["ccsr"] = ccsr
-
-        # # log our table with whatever we got to:
-        # print(e)
-        # wandb.log({"generations": wandb.Table(dataframe=pd.DataFrame(samples_d))})
-        # pass
+        # run ESMFold for generated sequences
+        input_fasta_file = outdir / "generated" / "sequences.fasta"
+        structure_outdir = outdir / "inverse_generated" / "structures"
+        inverse_generate_structure_run(
+            inverse_generate_structure_cfg,
+            fasta_file=input_fasta_file,
+            outdir=structure_outdir,
+            esmfold=esmfold,
+        )
 
     # ===========================
     # Phantom generations for self-consistency
     # ===========================
 
-    # run ProteinMPNN on the structure predictions of our generated sequences to look at self-consistency sequence recovery
-    if not (outdir / "phantom_generated").exists():
-        Path(outdir / "phantom_generated").mkdir(parents=True)
+    if cfg.run_phantom:
+        # run ProteinMPNN on the structure predictions of our generated sequences to look at self-consistency sequence recovery
+        if not (outdir / "phantom_generated").exists():
+            Path(outdir / "phantom_generated").mkdir(parents=True)
 
-    input_pdb_dir = outdir / "inverse_generated/structures"
-    output_fasta_path = outdir / "phantom_generated/sequences.fasta"
-    phantom_generate_sequence_run(
-        phantom_generate_sequence_cfg,
-        pdb_dir=input_pdb_dir,
-        output_fasta_path=output_fasta_path,
-    )
+        input_pdb_dir = outdir / "inverse_generated/structures"
+        output_fasta_path = outdir / "phantom_generated/sequences.fasta"
+        phantom_generate_sequence_run(
+            phantom_generate_sequence_cfg,
+            pdb_dir=input_pdb_dir,
+            output_fasta_path=output_fasta_path,
+        )
 
-    # uses OmegaFold to fold the inverse-fold sequence predictions of generated structures to look at scTM and scRMSD
-    phantom_generate_structure_run(outdir)
-
-    self_consistency_calc = SelfConsistencyEvaluation(outdir)
-
-    self_consistency_rmsd = self_consistency_calc.self_consistency_rmsd()
-    # self_consistency_rmspd = self_consistency_calc.cross_consistency_rmspd()
-    self_consistency_tm = self_consistency_calc.self_consistency_tm()
-    self_consistency_sr = self_consistency_calc.self_consistency_sr()
-
-    wandb.log(
-        {
-            # detailed stats for self-consistency RMSD and SR as representative metrics
-            "self_consistency_rmsd_mean": np.mean(self_consistency_rmsd),
-            "self_consistency_rmsd_std": np.std(self_consistency_rmsd),
-            "self_consistency_rmsd_median": np.median(self_consistency_rmsd),
-            "self_consistency_sr_mean": np.mean(self_consistency_sr),
-            "self_consistency_sr_std": np.std(self_consistency_sr),
-            "self_consistency_sr_median": np.median(self_consistency_sr),
-            # also log histogram
-            "self_consistency_tm_hist": wandb.Histogram(self_consistency_tm),
-            "self_consistency_rmsd_hist": wandb.Histogram(self_consistency_rmsd),
-            # "self_consistency_rmspd_hist": wandb.Histogram(self_consistency_rmspd),
-            "self_consistency_sr_hist": wandb.Histogram(self_consistency_sr),
-        }
-    )
-
-    # add to the big table
-    samples_d["self_consistency_rmsd"] = self_consistency_rmsd
-    # samples_d["self_consistency_rmspd"] = self_consistency_rmspd
-    samples_d["self_consistency_tm"] = self_consistency_tm
-    samples_d["self_consistency_sr"] = self_consistency_sr
-
-    # log our big table
-    wandb.log({"generations": wandb.Table(dataframe=pd.DataFrame(samples_d))})
+        # uses OmegaFold to fold the inverse-fold sequence predictions of generated structures to look at scTM and scRMSD
+        phantom_generate_structure_run(outdir)
 
 
 if __name__ == "__main__":
--- scripts/eval/evaluation_pipeline.sh
+++ scripts/eval/evaluation_pipeline.sh
@@ -1,5 +1,8 @@
-# take 1:32 40 48 56 64 72 80 88 96 104 112 120 128 136 144 152 160 168 176 184 192 200 208 216 224 232 240 248 256
-# take 2: 36 44 52 60 68 76 84 92 100 108 116 124 132 140 148 156 164 172 180 188 196 204 212 220 228 236 244 252
+len=48
+samples_dir=/data/lux70/plaid/artifacts/samples/5j007z42/100_200_300_v2/$len
+wandb_job_name=100_200_300_v2_$len
+
+sbatch consistency.slrm
 
 # arg_list=(32 40 48 56 64)
 # sbatch evaluation_pipeline.slrm "${arg_list[@]}"
@@ -32,14 +35,14 @@
 # sbatch evaluation_pipeline.slrm "${arg_list[@]}"
 
 
-arg_list=(180 184 188 192 196 200)
-sbatch evaluation_pipeline.slrm "${arg_list[@]}"
+# arg_list=(180 184 188 192 196 200)
+# sbatch evaluation_pipeline.slrm "${arg_list[@]}"
 
-arg_list=(204 208 212 216 220)
-sbatch evaluation_pipeline.slrm "${arg_list[@]}"
+# arg_list=(204 208 212 216 220)
+# sbatch evaluation_pipeline.slrm "${arg_list[@]}"
 
-arg_list=(224 228 232 236 240)
-sbatch evaluation_pipeline.slrm "${arg_list[@]}"
+# arg_list=(224 228 232 236 240)
+# sbatch evaluation_pipeline.slrm "${arg_list[@]}"
 
-arg_list=(244 248 252 256)
-sbatch evaluation_pipeline.slrm "${arg_list[@]}"
+# arg_list=(244 248 252 256)
+# sbatch evaluation_pipeline.slrm "${arg_list[@]}"
